ef69dba7548a222f6089d37726d46bde909e81f1
derek <derekthedevil69@gmail.com>
2024-07-27 14:45:44 +0530

    pratham

--- static/details.js
+++ static/details.js
@@ -1,7 +1,7 @@
 function myFunction() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=70d6a2e48ba55104a3a94cb2a5ba73a6`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=70d6a2e48ba55104a3a94cb2a5ba73a6&units=metric`)
         .then(response => response.json())
-        .then(result => console.log(result))
+        .then(result => details(result))
         .catch(error => console.log('error', error));
 }
 let lon;
@@ -11,3 +11,13 @@ let y = x.split("/").reverse()
 lon = y[1]
 lat = y[2]
 myFunction()
+
+let temp,feelslike,pressure,max,min,humidity,visiblity,wind_speed
+function details(result) {
+    temp = result['main']['temp']
+    feelslike = result['main']['feels_like']
+    pressure = result['main']['pressure']
+    max = result['main']['temp_max']
+    min = result['main']['temp_min']
+    
+}
